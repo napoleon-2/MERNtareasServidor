ff5c28e2e3adfec048c0e172901049b3d3eef9f6
napoleon geronimo <napoleongeronimo06@gmail.com>
2021-09-20 20:38:53 -0300

    verif auth nuevos proyectos,obt json, actualizar put , eliminar proyecto

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -2,17 +2,36 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controller/proyectoController');
 const auth = require('../middleware/auth')
+const { check } = require('express-validator');
 //crea proyectos
 //api/proyectos
 
 router.post('/',
     auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
     proyectoController.crearProyecto
 );
-
+//obtener todos los proyectos
 router.get('/',
     auth,
-    proyectoController.crearProyecto
+    proyectoController.obtenerProyectos
+    );
+//actualizar proyecto via ID 
+router.put('/:id',
+    auth,
+    [
+        check('nombre', 'El nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.actualizarProyecto
+    
+);
+
+//eliminar un proyecto
+router.delete('/:id',
+    auth,
+    proyectoController.eliminarProyecto
     );
 
 module.exports = router;
